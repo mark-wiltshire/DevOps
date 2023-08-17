990a545b567595da931f5b69b3aa18503ab6b86b
Bamboo Rocket Apps <mark@bamboorocketapps.com>
2023-08-17 17:52:08 -0500

    Minor updates to add own DB connection in backend_testing.py

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ PyCharm project requirements
 * pip install cryptography
 * pip install selenium
 
-Uses freesqldatabase.com for the MySQL Server
+Uses freesqldatabase.com for the MySQL Server connected to in the db_connector.py
 
 Run res_app.py to test REST API
-Utilises the db_connectory.py
-On Mac used https://apps.apple.com/gb/app/postcat-rest-api-testing/id1662268013?mt=12 to test
-Using MySQL to check data stored in DB
+* Utilises the db_connector.py
+* On Mac used https://apps.apple.com/gb/app/postcat-rest-api-testing/id1662268013?mt=12 to test
+* Using MySQL to check data stored in DB
 * GET - http://127.0.0.1:5000/users/<user_id> will return user_name
 * POST - http://127.0.0.1:5000/users/<user_id> with user_name payload - will set user_name
 * PUT - http://127.0.0.1:5000/users/<user_id> with user_name payload - will update user_name
@@ -27,13 +27,11 @@ Run web_app.py to test Web Interface
 Utilises the db_connectory.py
 * point browser to http://127.0.0.1:5001/users/get_user_data/1 
 
-Running frontend_testing.py
-This will test web interface 
-* you must edit file for location of: 
+Running frontend_testing.py - This will test Web interface 
+* web_app.py must be running
+* you must edit location of: 
 * A - chrome executable 
 * B - chromedriver 
-* web_app.py must be running
 
-Running backend_testing.py
-This will test REST API and Database Testing
+Running backend_testing.py - This will test REST API and Database Testing
 * rest_app.py must be running
--- backend_testing.py
+++ backend_testing.py
@@ -1,37 +1,60 @@
 # REST API and Database testing
+# res_app.py must be running
+import pymysql
 import requests
 
-import db_connector
-
-# initialise DB Connection
-db_connector.get_connection()
-print("DB Connection Opened")
-
-db_connector.init()
-print("DB initialised")
-
-#Set our user_name so we can test it later.
-USER_NAME = "mark"
+SCHEMA_NAME = "sql8640267"
+# Set our Test Data - user_id and user_name so we can test it later.
+TEST_USER_NAME = "mark"
+TEST_USER_ID = "22"
 
 # 1 - POST new user data (USER_ID hardcoded)
-res = requests.post('http://127.0.0.1:5000/users/22', json={"user_name":USER_NAME})
+res = requests.post('http://127.0.0.1:5000/users/'+TEST_USER_ID, json={"user_name": TEST_USER_NAME})
 if res.ok:
-    print(f"POST {res.json()}")
+    print(f"POST user_id[{TEST_USER_NAME}] {res.json()}")
 
 # 2 - Use GET to check status code 200 and data equals what was posted in 1
-res = requests.get('http://127.0.0.1:5000/users/22')
+res = requests.get('http://127.0.0.1:5000/users/'+TEST_USER_ID)
 if res.ok:
-    print(f"POST {res.json()}")
+    print(f"GET user_id[{TEST_USER_NAME}] {res.json()}")
 
 # 3 - Check data in the database
-# option 1 - using stored procedure - could also use new connector and SQL
-user_name_read = db_connector.read_user(22)
-if user_name_read == USER_NAME:
-    print(f"USER NAME CORRECT WRITTEN TO DB")
-else:
-    print(f"USER NAMES DIFFERENT DB = [{user_name_read}] SENT to REST API = [{USER_NAME}]")
+try:
+    print(f'Opening DB Connection')
+    db_connection = pymysql.connect(host='sql8.freesqldatabase.com', port=3306, user='sql8640267',
+                                        passwd='FkJQptHWtm', db=SCHEMA_NAME)
+    db_connection.autocommit(True)
+    # Getting a cursor from Database
+    db_cursor = db_connection.cursor()
+    print(f'Getting user_name from user_id [{TEST_USER_ID}]')
+    row_count = db_cursor.execute(f"Select user_name from {SCHEMA_NAME}.users where user_id = {TEST_USER_ID}")
+    print(f'row_count is [{row_count}]')
+    if row_count != 1:
+        print(f'Error row_count !=1 [{row_count}]')
+    else:
+        record = db_cursor.fetchone()
+        user_name_read = record[0]
+        if user_name_read == TEST_USER_NAME:
+            print(f"USER NAME CORRECT WRITTEN TO DB [{TEST_USER_NAME}] [{user_name_read}]")
+        else:
+            print(f"USER NAMES DIFFERENT DB = [{user_name_read}] SENT to REST API = [{TEST_USER_NAME}]")
+
+except pymysql.Error as e:
+    print(e)
 
 # EXTRA - run a delete to clean up after testing
-# therefore can run again.
-res = db_connector.delete_user(22)
-print(f"Cleaned up - Deleted user_id 22 [{res}]")
+# therefore we can run test again and again
+# TODO could better control db_cursor to check it was setup correctly before running
+try:
+    row_count = db_cursor.execute(
+        f"Delete from {SCHEMA_NAME}.users where user_id = {TEST_USER_ID}")
+    # row_count shows the number of rows effected.
+    print(f'row_count is [{row_count}]')
+    if row_count != 1:
+        print(f"Error when deleting user_id[{TEST_USER_ID}]")
+    else:
+        print(f"Cleaned up - Deleted user_id [{TEST_USER_ID}]")
+except pymysql.Error as e:
+    print(e)
+    print(f"SQL Error when deleting user_id[{TEST_USER_ID}]")
+
