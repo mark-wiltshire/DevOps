27ac2fda0def0a00564bf91ac52f79f712941482
Bamboo Rocket Apps <mark@bamboorocketapps.com>
2023-09-13 13:38:00 +0100

    DEPRECATED - was used for original Local Jenkins run NOT in Docker Version

--- clean_environment.py
+++ clean_environment.py
@@ -15,13 +15,14 @@ except requests.exceptions.RequestException as e:
     else:
         print(f"STOP SERVER Server wasn't running")
 
-try:
-    print(f"Stopping WEB API")
-    res = requests.get('http://127.0.0.1:5001/stop_server')
-except requests.exceptions.RequestException as e:
-    print(f"RequestException {e}")
-    test = e.args[0].args[0]
-    if "Connection aborted" in e.args[0].args[0]:
-        print(f"STOP SERVER WORKED")
-    else:
-        print(f"STOP SERVER Server wasn't running")
+# Now in Docker Not Using web_app
+# try:
+#     print(f"Stopping WEB API")
+#     res = requests.get('http://127.0.0.1:5001/stop_server')
+# except requests.exceptions.RequestException as e:
+#     print(f"RequestException {e}")
+#     test = e.args[0].args[0]
+#     if "Connection aborted" in e.args[0].args[0]:
+#         print(f"STOP SERVER WORKED")
+#     else:
+#         print(f"STOP SERVER Server wasn't running")
